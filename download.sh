340e8020590db30509880cfdeaddb9c73721fb92
Dan Rouse <dan@squarefoot.com>
2018-05-31 11:33:50 -0400

    update download script (node decompress uses too much memory)

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# [ `uname` == "Linux" ] || { echo "Only Linux OS is supported."; exit 1; }
+
+VERSION=18b
+OUT_DIR=source-data
+curl http://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/gdelx_$VERSION.zip -o source-data.zip
+unzip source-data.zip -d $OUT_DIR && rm source-data.zip
