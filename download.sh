eb5c0adb57ec69c8b94fe6aec98be2044972b060
Dan Rouse <dan@squarefoot.com>
2018-10-30 16:59:03 -0400

    follow redirects when dl data w/ cURL

--- download.sh
+++ download.sh
@@ -5,5 +5,5 @@ TMP_FILE_NAME=source-data.zip
 
 [ `uname` == "Linux" ] || { echo "Only Linux OS is supported."; exit 1; }
 [ -d "$OUT_DIR" ] && exit 0;
-[ -s "$TMP_FILE_NAME" ] || curl http://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/gdelx_$VERSION.zip -o "$TMP_FILE_NAME"
+[ -s "$TMP_FILE_NAME" ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/gdelx_$VERSION.zip -o "$TMP_FILE_NAME"
 unzip -nq "$TMP_FILE_NAME" -d "$OUT_DIR" && rm "$TMP_FILE_NAME"
