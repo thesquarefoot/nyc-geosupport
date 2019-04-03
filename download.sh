49b4478dad5f2ee06aaa4e9dc97dccba032519db
Dan Rouse <dan@squarefoot.com>
2019-04-03 14:53:46 -0400

    🤦‍♂️

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ OUT_DIR=source-data
 mkdir -p $OUT_DIR
 
 [ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo19a_191.zip -o geosupport-base.zip
-unzip -nq geosupport-base.zip -d "$OUT_DIR" && rm geosupport-base.zip 'version-19a_19.1/*'
+unzip -nq geosupport-base.zip -d "$OUT_DIR" 'version-19a_19.1/*' && rm geosupport-base.zip
 
 [ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_19a3.zip -o geosupport-data.zip
 unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
