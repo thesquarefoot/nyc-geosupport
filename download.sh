25afa5573f4644ab3282b1818fe870f7c4fc867f
Dan Rouse <dan@squarefoot.com>
2019-04-03 15:37:03 -0400

    mv after unzip to remove version from dirname

--- download.sh
+++ download.sh
@@ -2,10 +2,11 @@
 [ `uname` == "Linux" ] || { echo "Only Linux OS is supported."; exit 1; }
 
 OUT_DIR=source-data
-mkdir -p $OUT_DIR
+mkdir -p "$OUT_DIR"
 
 [ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo19a_191.zip -o geosupport-base.zip
-unzip -nq geosupport-base.zip -d "$OUT_DIR" 'version-19a_19.1/*' && rm geosupport-base.zip
+unzip -nq geosupport-base.zip && rm geosupport-base.zip
+mv version-19a_19.1/* -t "$OUT_DIR"
 
 [ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_19a3.zip -o geosupport-data.zip
 unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
