8edc209d1eb592cd6eb80421000cd76192fbb1e6
Dan Rouse <dan@squarefoot.com>
2019-04-03 14:43:27 -0400

    19a version bump

--- download.sh
+++ download.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-OUT_DIR=source-data
-TMP_FILE_NAME=source-data.zip
-
 [ `uname` == "Linux" ] || { echo "Only Linux OS is supported."; exit 1; }
+
+OUT_DIR=source-data
 mkdir -p $OUT_DIR
 
-[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/gdelx_18c.zip -o geosupport-base.zip
-unzip -nq geosupport-base.zip -d "$OUT_DIR" && rm geosupport-base.zip
+[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo19a_191.zip -o geosupport-base.zip
+unzip -nq geosupport-base.zip -d "$OUT_DIR" && rm geosupport-base.zip 'version-19a_19.1/*'
 
 [ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_19a3.zip -o geosupport-data.zip
-unzip -oq geosupport-data.zip -d "$OUT_DIR/version-18c_18.3/fls" && rm geosupport-data.zip
+unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
