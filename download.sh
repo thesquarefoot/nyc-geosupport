9a2c02f2d587aa1bebfba5fa3a1814e8906b3bfb
Doug Tabuchi <doug@dougtabuchi.com>
2020-12-10 14:11:11 -0500

    Use 20d

--- download.sh
+++ download.sh
@@ -4,9 +4,9 @@
 OUT_DIR=source-data
 mkdir -p "$OUT_DIR"
 
-[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo19a_191.zip -o geosupport-base.zip
+[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo20d_20.4.zip -o geosupport-base.zip
 unzip -nq geosupport-base.zip && rm geosupport-base.zip
-mv version-19a_19.1/* -t "$OUT_DIR"
+mv version-20d_20.4/* -t "$OUT_DIR"
 
-[ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_19a3.zip -o geosupport-data.zip
+[ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_20d1.zip -o geosupport-data.zip
 unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
