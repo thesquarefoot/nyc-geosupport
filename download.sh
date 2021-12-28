f924aa4ec8a5b311523fce449ae7af50015c0263
sforsell <sofia@squarefoot.com>
2021-12-28 11:22:30 +0100

    update zip file name

--- download.sh
+++ download.sh
@@ -4,9 +4,9 @@
 OUT_DIR=source-data
 mkdir -p "$OUT_DIR"
 
-[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo20d_20.4.zip -o geosupport-base.zip
+[ -s geosupport-base.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_geo21d_21_4.zip -o geosupport-base.zip
 unzip -nq geosupport-base.zip && rm geosupport-base.zip
 mv version-20d_20.4/* -t "$OUT_DIR"
 
-[ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_20d1.zip -o geosupport-data.zip
+[ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_21d2.zip -o geosupport-data.zip
 unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
