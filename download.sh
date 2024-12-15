318cf0d25bd8e31333748ec59c21c76755465619
Jordan Hamill <jordan-hamill@hotmail.com>
2024-12-15 21:27:40 +0000

    Change upad tpad files to one that exists

--- download.sh
+++ download.sh
@@ -8,5 +8,5 @@ mkdir -p "$OUT_DIR"
 unzip -nq geosupport-base.zip && rm geosupport-base.zip
 mv version-20d_20.4/* -t "$OUT_DIR"
 
-[ -s geosupport-data.zip ] || curl -L https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/linux_upad_tpad_20d1.zip -o geosupport-data.zip
+[ -s geosupport-data.zip ] || curl -L https://s-media.nyc.gov/agencies/dcp/assets/files/zip/data-tools/bytes/linux_upad_tpad_23d1.zip -o geosupport-data.zip
 unzip -oq geosupport-data.zip -d "$OUT_DIR/fls" && rm geosupport-data.zip
